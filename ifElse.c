d189d0662da57ef608f44ec70de83c2e6efde4af
vjaykrsna <vijay74684@gmail.com>
2024-03-23 23:54:35 +0530

    renaming files and deleting junk

---
+++ ifElse.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main(){
+    (1) ? printf("Hello World") : printf("failed");
+}
--- test.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    (1) ? printf("hello") : printf("failed");
-}
